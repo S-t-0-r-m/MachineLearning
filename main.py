bdca0c11a313a229e03cac295cf8daa195a8bce8
Sören Busselmann <s.buesselmann@outlook.de>
2022-11-01 21:28:27 +0100

    0.2

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 #  CRIM   ZN  INDUS  CHAS NOX  RM   AGE  DIS   RAD  TAX   PTRATIO  B   LSTAT  MEDV
 from app.mainwindow import MainWindowMain
-from PyQt5 import QtCore, QtWidgets
+from PyQt6 import QtCore, QtWidgets
 import sys
 import os
 
 PATH = os.path.dirname(__file__)
 
 def main():
-    mode = "app"
-    if mode == "app":
-        runapp()
 
 
-def runapp():
-    if hasattr(QtCore.Qt, "AA_EnableHighDpiScaling"):
-        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
-    if hasattr(QtCore.Qt, "AA_UseHighDpiPixmaps"):
-        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+    run_app()
 
+ 
+
+def run_app():
     root = QtWidgets.QApplication(sys.argv)
-    print(root.primaryScreen())
     window = MainWindowMain()
-    window.show()
 
-    sys.exit(root.exec_())
+    window.show()
+    sys.exit(root.exec())
 
 
 if __name__ == "__main__":
     main()
+
